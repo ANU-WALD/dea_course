aa23bc39e5fb3f9c0c94810950ceb0d239ebc64d
prl900 <prl900@users.noreply.github.com>
2020-08-28 03:37:37 +0000

    deploy: a89b7b3937e96ab8a3ba007d01702376ba25d26e

---
+++ dea_materials/day1/sync.sh
@@ -0,0 +1,12 @@
+#This script syncs the contents of day1 into the _notebooks contents:
+cp 1-intro_to_jupyter.ipynb ../../_notebooks/2020-08-12-intro_to_jupyter.ipynb
+cp 2-intro-to-numpy.ipynb ../../_notebooks/2020-08-11-intro-to-numpy.ipynb
+cp 3-intro-to-matplotlib.ipynb ../../_notebooks/2020-08-10-intro-to-matplotlib.ipynb
+cp 4-introduction-to-remote-sensing.ipynb ../../_notebooks/2020-08-09-introduction-to-remote-sensing.ipynb
+cp 5-basic-satellite-image-analysis.ipynb ../../_notebooks/2020-08-08-basic-satellite-image-analysis.ipynb
+cp 6-advanced_numpy.ipynb ../../_notebooks/2020-08-07-advanced_numpy.ipynb
+cp 7-intro_to_xarray.ipynb ../../_notebooks/2020-08-06-intro_to_xarray.ipynb
+
+cp check_answer.py ../../_notebooks/check_answer.py
+cp -r data/* ../../_notebooks/data/
+
